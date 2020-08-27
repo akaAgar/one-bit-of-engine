23ec6871b88b2ad4ef030160ccd87bb8c02b21d4
Agar <47314805+akaAgar@users.noreply.github.com>
2020-08-27 11:02:06 +0200

    Added a frame control to the test menu page

--- src/AsterionEngineDemo/TestMenuPage.cs
+++ src/AsterionEngineDemo/TestMenuPage.cs
@@ -18,6 +18,13 @@ namespace Asterion.Demo
             label.Text = "Hello world";
             label.Color = Color.CornflowerBlue;
             label.Position = new Point(3, 3);
+            label.ZOrder = 2;
+
+            MenuFrame frame = AddControl<MenuFrame>();
+            frame.Tile = (int)TileID.Font;
+            frame.Position = new Point(2, 2);
+            frame.Size = new Size(8, 8);
+            frame.ZOrder = 0;
         }
     }
 }
