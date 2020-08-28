75df31a1790097f7dff2e0f68d9b9dd7f3a33d99
Agar <47314805+akaAgar@users.noreply.github.com>
2020-08-28 13:31:53 +0200

    Moved Tile struct to root Asterion namespace

---
+++ src/AsterionEngine/Tile.cs
@@ -0,0 +1,26 @@
+﻿namespace Asterion
+{
+    public struct Tile
+    {
+        public int TileIndex { get; }
+        public RGBColor Color { get; }
+        public int Tilemap { get; }
+        public bool Animated { get; }
+
+        public Tile(int tileIndex, RGBColor color, int tilemap = 0, bool animated = false)
+        {
+            TileIndex = tileIndex;
+            Color = color;
+            Tilemap = tilemap;
+            Animated = animated;
+        }
+
+        public Tile(int tileIndex, RGBColor color, bool animated)
+        {
+            TileIndex = tileIndex;
+            Color = color;
+            Tilemap = 0;
+            Animated = animated;
+        }
+    }
+}
