def1288dddd7dfb3449a98093c6a61aedb82c558
Agar <47314805+akaAgar@users.noreply.github.com>
2020-08-28 11:42:24 +0200

    Added a skeleton of the InputManager class

---
+++ src/AsterionEngine/Input/InputManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asterion.Input
+{
+    public sealed class InputManager
+    {
+        internal void Dispose()
+        {
+
+        }
+    }
+}
