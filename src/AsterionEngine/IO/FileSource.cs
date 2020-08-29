0ef8a2c5187f27302dff5e3775148b4449664f62
Agar <47314805+akaAgar@users.noreply.github.com>
2020-08-29 11:34:57 +0200

    Added comments

--- src/AsterionEngine/IO/FileSource.cs
+++ src/AsterionEngine/IO/FileSource.cs
@@ -15,26 +15,121 @@ along with Asterion Engine. If not, see https://www.gnu.org/licenses/
 ==========================================================================
 */
 
+using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
 namespace Asterion.IO
 {
-    public abstract class FileSource
+    /// <summary>
+    /// Abstract class from which all "file source" classes, which store files to be used by the game, are derived.
+    /// </summary>
+    public abstract class FileSource : IDisposable
     {
-        internal abstract FileSystemSourceType SourceType { get; }
+        /// <summary>
+        /// Path to the file source. Can be a folder or a file, depending on the file source type.
+        /// </summary>
+        public string SourcePath { get; private set; }
 
-        internal string SourcePath { get; private set; }
-
-        internal FileSource(string path)
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="path">Path to the file or folder containing the files.</param>
+        public FileSource(string path)
         {
             SourcePath = path;
         }
 
-        internal abstract bool FileExists(string file);
+        /// <summary>
+        /// Does the file exist in this file source?
+        /// </summary>
+        /// <param name="file">Name of the file</param>
+        /// <returns>True if the file exists, false otherwise</returns>
+        public abstract bool FileExists(string file);
 
-        internal abstract byte[] GetFile(string file);
+        /// <summary>
+        /// Returns the bytes of a file stored in this file source.
+        /// </summary>
+        /// <param name="file">Name of the file</param>
+        /// <returns>An array of byte if the file exists, null otherwise</returns>
+        public abstract byte[] GetFile(string file);
 
-        internal virtual void Dispose()
+        /// <summary>
+        /// (Protected) Called when the source is disposed. Should be used to free memory and close open files.
+        /// </summary>
+        protected abstract void OnDispose();
+
+        /// <summary>
+        /// IDisposable implementation.
+        /// </summary>
+        public void Dispose() { OnDispose(); }
+
+        /// <summary>
+        /// (Protected) Returns some bytes to salt a password.
+        /// </summary>
+        /// <param name="password">The password to salt</param>
+        /// <returns>An array of bytes to use as salt</returns>
+        protected static byte[] GenerateSalt(string password)
         {
+            if (string.IsNullOrEmpty(password)) return new byte[0];
+            int seed = Encoding.UTF8.GetBytes(password).Select(x => (int)x).Sum();
+            Random saltRnd = new Random(seed);
 
+            byte[] salt = new byte[32];
+            for (int i = 0; i < salt.Length; i++)
+                salt[i] = (byte)saltRnd.Next(256);
+
+            return salt;
+        }
+
+        /// <summary>
+        /// (Protected) Encrypts an array of bytes.
+        /// </summary>
+        /// <param name="bytes">The array of bytes to encrypt</param>
+        /// <param name="password">Password to use for encryption</param>
+        /// <param name="passwordSalt">Bytes to use to salt the password</param>
+        /// <returns>An encrypted array of bytes</returns>
+        protected static byte[] EncryptBytes(byte[] bytes, string password, byte[] passwordSalt)
+        {
+            if (bytes == null) return null;
+            if (string.IsNullOrEmpty(password)) return bytes;
+            PasswordDeriveBytes pdb = new PasswordDeriveBytes(password, passwordSalt);
+
+            MemoryStream ms = new MemoryStream();
+            Aes aes = new AesManaged();
+            aes.Key = pdb.GetBytes(aes.KeySize / 8);
+            aes.IV = pdb.GetBytes(aes.BlockSize / 8);
+            CryptoStream cs = new CryptoStream(ms,
+              aes.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(bytes, 0, bytes.Length);
+            cs.Close();
+            return ms.ToArray();
+        }
+
+        /// <summary>
+        /// (Protected) Decrypts an array of bytes.
+        /// </summary>
+        /// <param name="bytes">The array of bytes to decrypt</param>
+        /// <param name="password">Password to use for decryption</param>
+        /// <param name="passwordSalt">Bytes to use to salt the password</param>
+        /// <returns>A decrypted array of bytes</returns>
+        protected static byte[] DecryptBytes(byte[] bytes, string password, byte[] passwordSalt)
+        {
+            if (bytes == null) return null;
+            if (string.IsNullOrEmpty(password)) return bytes;
+            PasswordDeriveBytes pdb = new PasswordDeriveBytes(password, passwordSalt);
+
+            MemoryStream ms = new MemoryStream();
+            Aes aes = new AesManaged();
+            aes.Key = pdb.GetBytes(aes.KeySize / 8);
+            aes.IV = pdb.GetBytes(aes.BlockSize / 8);
+            CryptoStream cs = new CryptoStream(ms,
+              aes.CreateDecryptor(), CryptoStreamMode.Write);
+            cs.Write(bytes, 0, bytes.Length);
+            cs.Close();
+            return ms.ToArray();
         }
     }
 }
--- src/AsterionEngine/IO/FileSourceFolder.cs
+++ src/AsterionEngine/IO/FileSourceFolder.cs
@@ -19,31 +19,56 @@ using System.IO;
 
 namespace Asterion.IO
 {
-    public sealed class FileSourceFolder : FileSource
+    /// <summary>
+    /// File source using a folder as the file source.
+    /// </summary>
+    public class FileSourceFolder : FileSource
     {
-        internal override FileSystemSourceType SourceType { get { return FileSystemSourceType.Folder; } }
-
-        internal FileSourceFolder(string path): base(path)
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="path">Path to the folder which stores the files</param>
+        public FileSourceFolder(string path): base(path)
         {
 
         }
 
+        /// <summary>
+        /// Creates an file path by combining the path to the folder used as the file source and the required file name.
+        /// </summary>
+        /// <param name="file">Relative path to the file from the root of the file source folder</param>
+        /// <returns>Path</returns>
         private string MakeFilePath(string file)
         {
             return Path.Combine(SourcePath, file);
         }
 
-        internal override bool FileExists(string file)
+        /// <summary>
+        /// Does the file exist in this file source?
+        /// </summary>
+        /// <param name="file">Name of the file</param>
+        /// <returns>True if the file exists, false otherwise</returns>
+        public override bool FileExists(string file)
         {
             return File.Exists(MakeFilePath(file));
         }
 
-        internal override byte[] GetFile(string file)
+        /// <summary>
+        /// Returns the bytes of a file stored in this file source.
+        /// </summary>
+        /// <param name="file">Name of the file</param>
+        /// <returns>An array of byte if the file exists, null otherwise</returns>
+        public override byte[] GetFile(string file)
         {
             string fileWithPath = MakeFilePath(file);
             if (!FileExists(fileWithPath)) return null;
 
             return File.ReadAllBytes(fileWithPath);
         }
+
+        /// <summary>
+        /// (Protected) OnDispose override. Does nothing as this kind of file source has nothing to close or dispose of.
+        /// </summary>
+        protected override void OnDispose() { }
     }
 }
