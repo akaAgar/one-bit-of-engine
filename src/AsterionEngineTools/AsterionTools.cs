e272307df28c598cfca201bd7b17ed57ce088e20
Agar <47314805+akaAgar@users.noreply.github.com>
2020-08-30 10:04:37 +0200

    Added new AsterionEngineTools Windows Forms project

---
+++ src/AsterionEngineTools/AsterionTools.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AsterionEngine.Tools
+{
+    public static class AsterionTools
+    {
+        /// <summary>
+        /// Main application entry point.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Forms.MainForm());
+        }
+    }
+}
