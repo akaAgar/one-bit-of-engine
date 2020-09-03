0717ec1653281c379e331094f7dbb726dbf62053
AgarCdF <ambroise.garel@gmail.com>
2020-09-03 16:37:52 +0200

    Finished controls on the VFX demo page

--- src/AsterionEngineDemo/UIPages/PageVFXDemo.cs
+++ src/AsterionEngineDemo/UIPages/PageVFXDemo.cs
@@ -7,15 +7,31 @@ namespace Asterion.Demo.UIPages
 {
     public sealed class PageVFXDemo : UIPage
     {
+        private static readonly int VFX_COUNT = AsterionTools.EnumCount<TileVFX>();
+
+        private UILabel LabelEffectDemo, LabelEffectName;
+        private TileVFX CurrentEffect = (TileVFX)1;
+
         protected override void OnInitialize(object[] parameters)
         {
             AddLabel(2, 2, "VISUAL FX DEMO", (int)TileID.Font, RGBColor.PaleGoldenrod);
 
-            UILabel label = AddLabel(2, 4, "Frame animation in the GLSL shader: ", (int)TileID.Font, RGBColor.White);
-            AddImage(2 + label.Text.Length, 4, 1, 1, (int)TileID.AnimationDemo, RGBColor.White);
-            AddImage(3 + label.Text.Length, 4, 1, 1, (int)TileID.Skeleton, RGBColor.White);
+            AddLabel(2, 4, "Frame animation in the GLSL shader: ", (int)TileID.Font, RGBColor.PaleGoldenrod);
+            AddImage(2, 5, 1, 1, (int)TileID.AnimationDemo, RGBColor.White);
+            AddImage(3, 5, 1, 1, (int)TileID.Skeleton, RGBColor.White);
 
+            LabelEffectName = AddLabel(2, 7, "", (int)TileID.Font, RGBColor.PaleGoldenrod);
+            LabelEffectDemo = AddLabel(2, 8, "This is a text with a special effect", (int)TileID.Font, RGBColor.White);
+            UpdateSelectedVFX();
+
+            AddLabel(2, UI.Game.Renderer.TileCount.Height - 4, "[Left], [Right]: change VFX", (int)TileID.Font, RGBColor.PaleGoldenrod);
             AddLabel(2, UI.Game.Renderer.TileCount.Height - 3, "[F]: fullscreen toggle, [ESC]: back", (int)TileID.Font, RGBColor.PaleGoldenrod);
+        }
+
+        private void UpdateSelectedVFX()
+        {
+            LabelEffectDemo.TileEffect = CurrentEffect;
+            LabelEffectName.Text = $"Effect {(int)CurrentEffect}/{VFX_COUNT - 1} ({CurrentEffect}):";
         }
 
         protected override void OnInputEvent(KeyCode key, ModifierKeys modifiers, int gamepadIndex, bool isRepeat)
@@ -24,6 +40,18 @@ namespace Asterion.Demo.UIPages
             {
                 case KeyCode.Escape:
                     UI.ShowPage<PageMainMenu>();
+                    return;
+
+                case KeyCode.Left:
+                    CurrentEffect--;
+                    if ((int)CurrentEffect < 1) CurrentEffect = (TileVFX)(VFX_COUNT - 1);
+                    UpdateSelectedVFX();
+                    return;
+
+                case KeyCode.Right:
+                    CurrentEffect++;
+                    if (CurrentEffect > (TileVFX)(VFX_COUNT - 1)) CurrentEffect = (TileVFX)1;
+                    UpdateSelectedVFX();
                     return;
             }
         }
