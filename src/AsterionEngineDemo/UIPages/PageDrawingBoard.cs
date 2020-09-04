7f39ccfdf3680ec53910e7af5aca480b1030a918
Agar <47314805+akaAgar@users.noreply.github.com>
2020-09-04 09:18:27 +0200

    Added TileBoard control with frame

--- src/AsterionEngineDemo/UIPages/PageDrawingBoard.cs
+++ src/AsterionEngineDemo/UIPages/PageDrawingBoard.cs
@@ -7,24 +7,48 @@ namespace Asterion.Demo.UIPages
 {
     public sealed class PageDrawingBoard : UIPage
     {
+        private static readonly Position BOARD_POSITION = new Position(2, 4);
+        private static readonly Dimension BOARD_SIZE = new Dimension(16, 16);
+
+        private UITileBoard TileBoard;
+
         protected override void OnInitialize(object[] parameters)
         {
-            UIFrame frame = AddFrame(
-                0, 0, UI.Game.Renderer.TileCount.Width, UI.Game.Renderer.TileCount.Height,
-                (int)TileID.Frame, RGBColor.Goldenrod);
-            frame.ZOrder = 1;
+            UI.Cursor.Enabled = true;
+            UI.Cursor.Position = BOARD_POSITION;
+            UI.Cursor.BoundingBox = new Area(BOARD_POSITION, BOARD_SIZE);
+            UI.Cursor.Moveable = true;
 
-            AddLabel(2, UI.Game.Renderer.TileCount.Height - 3, "[F]: fullscreen toggle, [ESC]: back", (int)TileID.Font, RGBColor.PaleGoldenrod);
+            AddLabel(1, 1, "DRAWING BOARD", (int)TileID.Font, RGBColor.PaleGoldenrod);
+
+            AddFrame(
+                BOARD_POSITION - Position.One, BOARD_SIZE + Dimension.One * 2,
+                (int)TileID.Frame, RGBColor.White);
+
+            TileBoard = AddTileBoard(BOARD_POSITION.X, BOARD_POSITION.Y, BOARD_SIZE.Width, BOARD_SIZE.Height);
+            TileBoard.Clear(new UITileBoardTile(1, RGBColor.Blue));
+
+            AddLabel(2, UI.Game.Renderer.TileCount.Height - 4, "Arrow keys, gamepad sticks/DPad: move cursor", (int)TileID.Font, RGBColor.PaleGoldenrod);
+            AddLabel(2, UI.Game.Renderer.TileCount.Height - 3, "F: fullscreen toggle, ESC: back", (int)TileID.Font, RGBColor.PaleGoldenrod);
         }
 
         protected override void OnInputEvent(KeyCode key, ModifierKeys modifiers, int gamepadIndex, bool isRepeat)
         {
             switch (key)
             {
+                case KeyCode.Space:
+                    Position boardPosition = UI.Cursor.Position - BOARD_POSITION;
+                    TileBoard[boardPosition] = new UITileBoardTile(2, RGBColor.BurlyWood);
+                    return;
                 case KeyCode.Escape:
                     UI.ShowPage<PageMainMenu>();
                     return;
             }
+        }
+
+        protected override void OnClose()
+        {
+            UI.Cursor.Enabled = false;
         }
     }
 }
